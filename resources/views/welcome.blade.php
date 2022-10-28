f9034861c110233af35f81d6bbeac60e6228779a
LK333 <75745490+LK333@users.noreply.github.com>
2022-10-29 02:53:46 +0400

    Added style, fixed form position.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
     <title>Document</title>
 </head>
-<body>
+<br>
 <form action="add" method="POST">
     @csrf
-    <div><label for="name">Name:</label>
+    <div><label for="name" >Name:</label>
         <input type="text" name="name">
     </div>
     <div>
@@ -21,16 +22,17 @@
         <label for="message">Message:</label>
         <textarea name="message"></textarea>
     </div>
-    <button>Send</button>
-    <?php
-    foreach ($messages as $message) {
-    ?>
-    <div><?=$message->name?></div>
-    <div><?=$message->email?></div>
-    <div><?=$message->message?></div>
-    <?php
-    }
-    ?>
+    <div><button>Submit</button></div>
 </form>
+<?php
+foreach ($messages as $message) {
+?>
+<div>Name: <?=$message->name?></div>
+<div>Email: <?=$message->email?></div>
+<div>Message: <?=$message->message?></div>
+</br>
+<?php
+}
+?>
 </body>
 </html>
